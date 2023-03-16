79642079e5642999a6f37672f69e531fe45f6c69
suprabathk <suprabathkondapally@gmail.com>
2023-03-16 17:33:05 +0530

    Changed logo font from sans to serif

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="Navbar mb-10 py-4 border-b border-gray-600 bg-black">
             <div className="flex justify-between  lg:mx-60 md:mx-40 mx-10 items-center">
-                <Link to="/" className="text-2xl font-medium text-purple-500">The Times</Link>
+                <Link to="/" className="text-3xl font-medium text-purple-500 font-serif">The Times</Link>
                 <div className="flex gap-8 justify-between text-white">
                     <Link className="hover:text-purple-600 hover:border-purple-600 transition-all ease-linear border rounded-md px-3 py-1" to="/">Home</Link>
                     <Link className="hover:text-purple-600 hover:border-purple-600 transition-all ease-linear border rounded-md px-3 py-1" to="/create">New Blog</Link>
